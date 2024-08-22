9c45be1ad5c73c6d1c4086af13c15b532b6e7095
HRan2004 <2125764918@qq.com>
2024-08-22 23:20:18 +0800

    feat: 切分pdf为多个txt

---
+++ src/split/read.py
@@ -0,0 +1,32 @@
+import fitz
+
+page_list = {
+  '摘要': 1, '北京': 102, '天津': 140, '河北': 157, '山西': 185, '内蒙古': 226, '上海': 302, '江苏': 464, '浙江': 530,
+  '安徽': 604, '山东': 676, '福建': 758, '广东': 833, '广西': 979, '海南': 1001, '湖北': 1038, '湖南': 1078,
+  '河南': 1102, '江西': 1153, '吉林': 1230, '黑龙江': 1283, '辽宁': 1295, '四川': 1316, '重庆': 1376,
+  '贵州': 1409, '云南': 1432, '西藏': 1479, '陕西': 1479, '宁夏': 1558, '甘肃': 1599, '青海': 1704, '新疆': 1704
+}
+keys = list(page_list.keys())
+
+pdf_document = '../../data/business.pdf'
+doc = fitz.open(pdf_document)
+for city in page_list.keys():
+    target_file = f'../../data/split/{city}.txt'
+    start = page_list.get(city)
+    end_i = keys.index(city) + 1
+    end = page_list[keys[end_i]] if end_i < len(keys) else doc.page_count
+    print(f'Start analysis {city}.txt     From {start}~{end}')
+    
+    text = ''
+    for page_num in range(start - 1, end):
+        text += f'\n# Page {page_num + 1}:\n'
+        blocks = doc[page_num].get_text("dict")["blocks"]
+        for block in blocks:
+            if "lines" in block:
+                for line in block["lines"]:
+                    for span in line["spans"]:
+                        if span['size'] > 9.1 and span['font'] != 'TimesNewRomanPSMT':
+                            text += span['text'] + '\n'
+    open(target_file, 'w', encoding='utf-8').write(text)
+    
+print('Finished')
--- src/split/split.py
+++ src/split/split.py
@@ -8,12 +8,12 @@ doc = fitz.open(pdf_document)
 # 提取文本
 text = ""
 for page_num in range(len(doc)):
-    page = doc.load_page(page_num)  # 加载页
-    page_text = page.get_text()  # 提取文本page_
-    print(page_text)
-    text += page_text
-    if page_num > 22:
-        break
+  page = doc.load_page(page_num)  # 加载页
+  page_text = page.get_text()  # 提取文本page_
+  print(page_text)
+  text += page_text
+  if page_num > 22:
+    break
 
 catalog_str = text
 
@@ -26,13 +26,15 @@ matches = pattern.findall(catalog_str)
 # 构建字典
 result = {}
 for match in matches:
-    region = match[0].strip()
-    page = int(match[1])
-    result[region] = page
+  region = match[0].strip()
+  page = int(match[1])
+  result[region] = page
 
 print(result)
 
-result = {'北京': 102, '天津': 140, '河北': 157, '山西': 185, '内蒙古': 226, '上海': 302, '江苏': 464, '浙江': 530, '安徽': 604, '山东': 676, '福建': 758, '广东': 833, '广西': 979, '海南': 1001, '湖北': 1038, '湖南': 1078, '河南': 1102, '江西': 1153, '吉林': 1230, '黑龙江': 1283, '辽宁省': 1295, '四川': 1316, '重庆': 1376, '贵州': 1409, '云南': 1432, '西藏': 1479, '陕西': 1479, '宁夏': 1558, '甘肃': 1599, '青海': 1704, '新疆': 1704}
-
-
-
+# result = {
+#   '北京': 102, '天津': 140, '河北': 157, '山西': 185, '内蒙古': 226, '上海': 302, '江苏': 464, '浙江': 530,
+#   '安徽': 604, '山东': 676, '福建': 758, '广东': 833, '广西': 979, '海南': 1001, '湖北': 1038, '湖南': 1078,
+#   '河南': 1102, '江西': 1153, '吉林': 1230, '黑龙江': 1283, '辽宁省': 1295, '四川': 1316, '重庆': 1376,
+#   '贵州': 1409, '云南': 1432, '西藏': 1479, '陕西': 1479, '宁夏': 1558, '甘肃': 1599, '青海': 1704, '新疆': 1704
+# }
